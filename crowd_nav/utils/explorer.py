10fff489b3c98c6adb1d5115432e5edf3e4a5580
MMustafa01 <sm06554@st.habib.edu.pk>
2024-02-24 17:41:46 +0000

    Before omema starts

--- crowd_nav/utils/explorer.py
+++ crowd_nav/utils/explorer.py
@@ -73,7 +73,7 @@ class Explorer(object):
                 if isinstance(info, ReachGoal) or isinstance(info, Collision):
                     # only add positive(success) or negative(collision) experience in experience set
                     self.update_memory(states, actions, rewards, imitation_learning)
-                tqdm.write(f'Experience set size: {self.memory}/{self.memory.capacity}')
+                tqdm.write(f'Experience set size: {len(self.memory)}/{self.memory.capacity}')
             cumulative_rewards.append(sum([pow(self.gamma, t * self.robot.time_step * self.robot.v_pref)
                                            * reward for t, reward in enumerate(rewards)]))
 
